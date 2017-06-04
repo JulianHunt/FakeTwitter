f00332ae78fce1e76f6db2e323f20362a1a2425f
Julian <julianhuntwow@hotmail.com>
2017-06-03 20:05:17 -0400

    Adds empty TabBar and TabBarViewController

---
+++ FakeTwitter/HomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  HomeViewController.m
+//  FakeTwitter
+//
+//  Created by Julian Hunt on 2017-06-03.
+//  Copyright © 2017 Julian Hunt. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
